ba7c7d8d25193b171419b9773ddeeee80fed2b2d
Andrey Rubtsov <aa.rubtsov@icloud.com>
2025-07-28 15:59:03 +0300

    first commit

---
+++ task_2/index.php
@@ -0,0 +1,9 @@
+<?php
+$fullName = 'Иванов Иван Иванович';
+$fio = 'ИИИ';
+$surnameAndInitials = 'Иванов И.И.';
+
+echo "Полное имя: " . $fullName . PHP_EOL;
+echo "Фамилия и инициалы: " .  $fio . PHP_EOL;
+echo "Аббревиатура: " .  $surnameAndInitials . PHP_EOL;
+?>
