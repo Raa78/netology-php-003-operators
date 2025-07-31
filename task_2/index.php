ace92ffa88571e533e8994281200e5ff4e0616e6
Andrey Rubtsov <aa.rubtsov@icloud.com>
2025-07-31 15:17:15 +0300

    final v1.0

--- task_2/index.php
+++ task_2/index.php
@@ -23,9 +23,7 @@ function requestPersonalData()
     foreach ($listRequests as $key => $value) {
 
         echo $value . PHP_EOL;
-
         $data =  ucfirst(trim(fgets(STDIN)));
-        echo "поток>>>" . $data . PHP_EOL;
 
         // Для инициалов
         $slice = mb_substr($data, 0, 1, "utf-8");
@@ -53,16 +51,25 @@ echo "Фамилия и инициалы: " . $customerCard["surnameAndInitials"
 echo "Аббревиатура: " .  $customerCard["fio"] . PHP_EOL;
 
 
-echo "Второй вариант вывода -  упрощенный"
 
-// $fullName = 'Иванов Иван Иванович';
-// $fio = 'ИИИ';
-// $surnameAndInitials = 'Иванов И.И.';
+//ВАРИАНТ 2
+echo "Второй вариант вывода -  упрощенный" . PHP_EOL;
 
-// echo "Полное имя: " . $fullName . PHP_EOL;
-// echo "Фамилия и инициалы: " .  $fio . PHP_EOL;
-// echo "Аббревиатура: " .  $surnameAndInitials . PHP_EOL;
+echo "Введите Фамилию" . PHP_EOL;
+$lastName = ucfirst(trim(fgets(STDIN)));
+
+echo "Введите Имя" . PHP_EOL;
+$firstName = ucfirst(trim(fgets(STDIN)));
+
+echo "Введите Отчество" . PHP_EOL;
+$middleName = ucfirst(trim(fgets(STDIN)));
 
 
+$fullName = $lastName . " " . $firstName . " " . $middleName;
+$fio = substr($lastName, 0, 1) . substr($firstName, 0, 1) . substr($middleName, 0, 1);
+$surnameAndInitials = $lastName . " " . substr($firstName, 0, 1) . "." . substr($middleName, 0, 1) . ".";
 
+echo "Полное имя: " . $fullName . PHP_EOL;
+echo "Фамилия и инициалы: " .  $surnameAndInitials . PHP_EOL;
+echo "Аббревиатура: " .  $fio . PHP_EOL;
 ?>
